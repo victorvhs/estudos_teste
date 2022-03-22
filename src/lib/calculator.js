48aa86c45529cfeae0d815cf65188d9ea277d738
Victor Reis <victor.h.s.reis@gmail.com>
2022-03-21 22:07:45 -0300

    complentando os teste com TDD

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,1 +1,9 @@
-module.exports.sum = (n1, n2) => n1 + n2;
+module.exports.sum = (n1, n2) => {
+  const a = parseInt(n1, 10);
+  const b = parseInt(n2, 10);
+
+  if (isNaN(a) || isNaN(b)) {
+    throw new Error("Números inválidos");
+  }
+  return a + b;
+};
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -3,3 +3,28 @@ const { sum } = require("./calculator");
 it("A soma entre 2+2 deve ser 4 ", () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+it("A soma entre 2+2 deve ser 4 mesmo se for uma string ", () => {
+  expect(sum("2", "2")).toBe(4);
+});
+
+it("Se a soma não puder acontecer deve retornar um erro", () => {
+  // Se o teste for relacionando ao mesmo caso eles podem ficar dentro do mesmo bloco
+  //Isso deixa a implementação mais coerente.
+	
+  expect(() => {
+    sum("", "2");
+  }).toThrowError();
+
+  expect(() => {
+    sum([2, 2]);
+  }).toThrowError();
+
+  expect(() => {
+    sum({});
+  }).toThrowError();
+
+  expect(() => {
+    sum();
+  }).toThrowError();
+});
